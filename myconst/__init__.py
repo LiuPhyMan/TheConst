fb226fb383ee7aac2aa1be5aefe4557e1a93fc61
LiuPhyMan <liu.jinbao@outlook.com>
2023-06-29 04:15:39 +0800

    Initial commit

---
+++ myconst/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Liu.Jinbao
+@contact: liu.jinbao@outlook.com
+@time: 23.May.2023
+"""
+
+from scipy.constants import *
+eV2J = e
+J2eV = 1/eV2J
+K2J = k
+J2K = 1/K2J
+eV2K = eV2J * J2K
+K2eV = 1/eV2K
+relM2absM = atomic_mass
+absM2relM = 1 / relM2absM
+e2 = 2.30708e-28 # unit: J m
+
+def nm2Hz(wavelength):
+    return 1e9 * c / wavelength
---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "myconst",
+    packages = find_packages(),
+    version = "0.1.0",
+    install_requires=[
+        "scipy"
+    ]
+)
